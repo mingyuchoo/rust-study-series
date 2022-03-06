74ff607972e755ce98819ae521bc620ee52bf8c8
mingyuchoo <mingyuchoo@gmail.com>
2022-03-07 04:34:14 +0900

    added

--- the-rust-programming-language/p17_smart_pointer/src/main.rs
+++ the-rust-programming-language/p17_smart_pointer/src/main.rs
@@ -1,6 +1,9 @@
 mod boxes;
 mod derefs;
 mod drops;
+mod rcs;
+mod refcells;
+mod memory_leak;
 
 fn main() {
   boxes::call1();
@@ -10,4 +13,11 @@ fn main() {
 
   drops::call1();
   drops::call2();
+
+  rcs::call1();
+
+  refcells::call1();
+
+  memory_leak::call1();
+  memory_leak::call2();
 }
---
+++ the-rust-programming-language/p17_smart_pointer/src/memory_leak.rs
@@ -0,0 +1,63 @@
+use std::rc::{Rc, Weak};
+use std::cell::RefCell;
+use List::{Cons, Nil};
+
+#[derive(Debug)]
+enum List {
+  Cons(i32, RefCell<Rc<List>>),
+  Nil,
+}
+impl List {
+  fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+    match *self {
+      Cons(_, ref item) => Some(item),
+      Nil => None,
+    }
+  }
+}
+
+pub fn call1() {
+  let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+  println!("a의 최초 rc 카운트 = {}", Rc::strong_count(&a));
+  println!("a의 다음 아이템 = {:?}", a.tail());
+
+  let b = Rc::new(Cons(5, RefCell::new(Rc::clone(&a))));
+  println!("b를 생성한 뒤 a의 rc 카우트 = {}", Rc::strong_count(&a));
+  println!("b의 최초 rc 카운트 = {}", Rc::strong_count(&b));
+  println!("b의 다음 아이템 = {:?}", b.tail());
+
+  if let Some(link) = a.tail() {
+    *link.borrow_mut() = Rc::clone(&b);
+  }
+
+  println!("a를 변경한 뒤 b의 rc 카운트 = {}", Rc::strong_count(&b));
+  println!("a를 변경한 뒤 a의 rc 카운트 = {}", Rc::strong_count(&a));
+}
+
+#[derive(Debug)]
+struct Node {
+  value: i32,
+  parent: RefCell<Weak<Node>>,
+  children: RefCell<Vec<Rc<Node>>>,
+}
+
+pub fn call2() {
+  let leaf = Rc::new(Node {
+    value: 3,
+    parent: RefCell::new(Weak::new()),
+    children: RefCell::new(vec![]),
+  });
+  println!("leaf strong= {}, weak = {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+  {
+    let branch = Rc::new(Node {
+      value: 5,
+      parent: RefCell::new(Weak::new()),
+      children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    println!("branch strong = {}, weak = {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+    println!("leaf strong = {}, weak = {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+  }
+  println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+  println!("leaf strong = {}, weak = {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+}
