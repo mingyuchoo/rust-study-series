11cb3db9150c2b06b47870c97dbdac734cee8d96
mingyuchoo <mingyuchoo@gmail.com>
2022-08-23 06:15:44 +0900

    formatted

--- the-rust-programming-language/p01_hello_world/main.rs
+++ the-rust-programming-language/p01_hello_world/main.rs
@@ -1,22 +1,20 @@
 fn main() {
-  println!("{}, {}!", "Hello", "world"); // Hello, world!
+  println!("{}, {}!", "Hello", "world");   // Hello, world!
   println!("{0}, {1}!", "Hello", "world"); // Hello, world!
 
-  println!("{:?}", [1, 2, 3]); // [1, 2, 3]
+  println!("{:?}", [1, 2, 3]);             // [1, 2, 3]
   println!("{:#?}", [1, 2, 3]); 
   /*
   [
-    1,
-    2,
-    3
+      1,
+      2,
+      3
   ]
   */
   let x = format!("{}, {}!", "Hello", "world");
-  println!("{}", x); // Hello, world!
-
-  print!("Hello, world!"); // Without new line
-  println!(); // A new line
-
-  println!("Hello, world!\n"); // With new line
+  println!("{}", x);             // Hello, world!
+  print!("Hello, world!");       // Without new line
+  println!();                    // A new line
+  println!("Hello, world!\n");   // With new line
 }
 
