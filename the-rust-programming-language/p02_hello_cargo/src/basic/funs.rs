d2d3cd727053facb80e4884736c4309ed11bffde
mingyuchoo <mingyuchoo@gmail.com>
2022-08-23 06:31:47 +0900

    formatted

--- the-rust-programming-language/p02_hello_cargo/src/basic/funs.rs
+++ the-rust-programming-language/p02_hello_cargo/src/basic/funs.rs
@@ -3,24 +3,25 @@ pub fn main() {
 
     // 01. Without type declarations.
     let p1 = plus_one;
-    let x = p1(5); // 6
-    println!("{}", x);
+    let x = p1(5);
+    println!("{}", x); // 6
 
     // 02. With type declarations.
     // and using variable shadowing
     let p1: fn(i32) -> i32 = plus_one;
-    let x = p1(5); // 6
-    println!("{}", x);
+    let x = p1(5);
+    println!("{}", x); // 6
 
     // 03. Call other function
     let p2: fn(i32) -> i32 = plus_two;
-    let y = p2(5); // 7
-    println!("{}", y);
+    let y = p2(5);
+    println!("{}", y); // 7
 
     let x = 2;
     println!("{}", get_square_value(x));
+
+    // Input parameters are passed inside | | and expression body is wrapped within { }
     let square = |i: i32| -> i32 {
-        // Input parameters are passed inside | | and expression body is wrapped within { }
         i * i
     };
     println!("{}", square(x)); // 4
@@ -32,7 +33,7 @@ pub fn hello() -> String {
     ("Hello, world!").to_string()
 }
 
-/// 01. Without the return keyword. Only the lask expression returns
+/// 01. Without the return keyword. Only the last expression returns
 pub fn plus_one(a: i32) -> i32 {
     a + 1
     // There is no ending ; in the above line.
@@ -42,7 +43,7 @@ pub fn plus_one(a: i32) -> i32 {
 /// 02. With the return keyword.
 pub fn plus_two(a: i32) -> i32 {
     return a + 2;
-    // Should use return keyword only on conditional/ early returns.
+    // Should use return keyword only on conditional / early returns.
     // Using return keyword in the last expression is a bad practice.
 }
 
