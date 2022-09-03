6271b4f5f6913aecfd633483d848533f342cc6a1
mingyuchoo <mingyuchoo@gmail.com>
2022-09-03 14:52:59 +0900

    added

---
+++ graphql/async_graphql_v40/README.md
@@ -0,0 +1,16 @@
+# async_graphql_v40
+
+## Reference
+
+- https://async-graphql.github.io/async-graphql/en/quickstart.html
+- https://github.com/async-graphql/examples
+- https://github.com/async-graphql/async-graphql
+
+
+## GraphQL Query for test
+
+```graphql
+query {
+ add(a: 10, b: 20)
+}
+```
--- graphql/async_graphql_v40/src/main.rs
+++ graphql/async_graphql_v40/src/main.rs
@@ -1,48 +1,47 @@
 use actix_web;
 use async_graphql;
+use async_graphql_actix_web;
 
-struct Query;
+struct QueryRoot;
 
 #[async_graphql::Object]
-impl Query {
+impl QueryRoot {
     async fn add(&self, a: i32, b: i32) -> i32 {
         a + b
     }
 }
 
 
-#[actix_web::get("/demo")]
-async fn demo() -> impl actix_web::Responder {
-    let schema = async_graphql::Schema::new(Query, async_graphql::EmptyMutation, async_graphql::EmptySubscription);
-    let res = schema.execute("{ add(a: 10, b: 20) }").await;
-
-    actix_web::web::Json(res)
+async fn graphiql() -> actix_web::HttpResponse {
+    actix_web::HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(
+            async_graphql::http::GraphiQLSource::build()
+                .endpoint("http://localhost:4000")
+                .subscription_endpoint("ws://localhost:4000/ws")
+                .finish(),
+        )
 }
 
-
-#[actix_web::get("/")]
-async fn hello() -> impl actix_web::Responder {
-    actix_web::HttpResponse::Ok().body("Hello world!")
-}
-
-
-
-#[actix_web::post("/echo")]
-async fn echo(req_body: String) -> impl actix_web::Responder {
-    actix_web::HttpResponse::Ok().body(req_body)
+async fn index(
+    schema: actix_web::web::Data<async_graphql::Schema<QueryRoot, async_graphql::EmptyMutation, async_graphql::EmptySubscription>>,
+    req: async_graphql_actix_web::GraphQLRequest,
+) -> async_graphql_actix_web::GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
 }
 
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    actix_web::HttpServer::new(|| {
+
+    actix_web::HttpServer::new(move || {
         actix_web::App::new()
-            .service(demo)
-            .service(hello)
-            .service(echo)
+            .app_data(actix_web::web::Data::new(async_graphql::Schema::new(QueryRoot, async_graphql::EmptyMutation, async_graphql::EmptySubscription)))
+            .service(actix_web::web::resource("/").guard(actix_web::guard::Get()).to(graphiql))
+            .service(actix_web::web::resource("/").guard(actix_web::guard::Post()).to(index))
     })
-    .bind(("127.0.0.1", 4000))?
+    .bind("127.0.0.1:4000")?
     .run()
     .await
 }
