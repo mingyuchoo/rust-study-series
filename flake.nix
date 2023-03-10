b7f2518c6ba00ba0fbf28c513ae6eac2688d2b61
mingyuchoo <mingyuchoo@gmail.com>
2023-03-10 10:56:48 +0900

    improved

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
               rustup
             ];
             shellHook = ''
+              export LANG=C.UTF-8
               export EDITOR=emacs
               eval "$(direnv hook bash)"
               echo "Welcome to nix flake for Rust"
