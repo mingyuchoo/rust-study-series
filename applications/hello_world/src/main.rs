b7a484533af8338424a128205b60de63741d7827
mingyuchoo <mingyuchoo@gmail.com>
2023-07-05 05:51:55 +0900

    added tests

--- applications/hello_world/src/main.rs
+++ applications/hello_world/src/main.rs
@@ -1,8 +1,28 @@
 use regex::Regex;
 
-fn main() -> () {
-    let re: Regex = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-    println!("Did our date match? {}", re.is_match("2014-01-01"));
+fn main() {
+    let date: &str = "2023-07-05";
+    println!("Did our date match? {}", check_date(date));
+}
 
-    ()
+fn check_date(date: &str) -> bool {
+    let re: Regex = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    re.is_match(date)
+}
+
+#[cfg(test)]
+mod tests_check_date {
+    use crate::*; // or `use super::*;`
+
+    #[test]
+    fn happy_path_1() {
+        let date: &str = "2023-07-05";
+        assert_eq!(true, check_date(date), "should be match YYYY-MM-DD");
+    }
+
+    #[test]
+    fn unhappy_path_1() {
+        let date: &str = "20230705";
+        assert_eq!(false, check_date(date), "should NOT be match YYYYMMDD");
+    }
 }
