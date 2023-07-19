12d07790a4235d1034a5af12d59ddb6c3c962417
mingyuchoo <mingyuchoo@gmail.com>
2023-07-20 05:23:48 +0900

    re-organized

---
+++ aws/aws_sdk_dynamodb_v028/src/main.rs
@@ -0,0 +1,24 @@
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_dynamodb::{Client, Error};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let region_provider = RegionProviderChain::default_provider().or_else("ap-northeast-2");
+    let config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&config);
+
+    let resp = client.list_tables().send().await?;
+
+    println!("Tables:");
+
+    let names = resp.table_names().unwrap_or_default();
+
+    for name in names {
+        println!("   {}", name);
+    }
+
+    println!();
+    println!("Found {} tables", names.len());
+
+    Ok(())
+}
