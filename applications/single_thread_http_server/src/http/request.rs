f3630d9ae50f0b8b823173634aa55e44cc28972e
mingyuchoo <mingyuchoo@gmail.com>
2023-08-14 05:00:59 +0900

    if-let expression

--- applications/single_thread_http_server/src/http/request.rs
+++ applications/single_thread_http_server/src/http/request.rs
@@ -10,13 +10,39 @@ use std::convert::TryFrom;
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
-    // GET /search?name=abc&sort=1 HTTP/1.1
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         use std::str;
         let request = str::from_utf8(buf)?;
 
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocal);
+        }
+
+        let method: Method = method.parse()?;
+
+        let mut query_string = None;
+
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
+        }
+
         unimplemented!()
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &&request[i + 1..]));
+        }
+    }
+    None
 }
 
 #[allow(dead_code)]
@@ -56,6 +82,14 @@ impl Display for ParseError {
 }
 
 use std::convert::From;
+
+use super::method::MethodError;
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
 use std::str::Utf8Error;
 impl From<Utf8Error> for ParseError {
     fn from(_: Utf8Error) -> Self {
