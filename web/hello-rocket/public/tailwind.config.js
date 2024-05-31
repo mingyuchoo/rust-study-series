336af29c60f679df394c16e6577b056211570403
mingyuchoo <mingyuchoo@gmail.com>
2024-05-31 15:11:21 +0900

    updated

--- web/hello-rocket/public/tailwind.config.js
+++ web/hello-rocket/public/tailwind.config.js
@@ -1,42 +1,13 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	purge: ['./html/**/*.html'],
-  content: [],
-  theme: {
-    colors: {
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
-    extend: {
-      spacing: {
-        "8xl": "96rem",
-        "9xl": "128rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-    },
-  },
-  variants: {
-    extend: {
-      animation: ['group-hover']
-    },
-  },
+  content: ['./html/**/*.html'],
+  theme: {},
   plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
-    require("tailwindcss-children"),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-children'),
   ],
 };
