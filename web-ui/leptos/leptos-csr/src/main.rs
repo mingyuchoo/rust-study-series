2ac3445f5ed4f3a91bd3a3b0cbfd9c396370a35b
mingyuchoo <mingyuchoo@gmail.com>
2024-11-07 21:08:47 +0900

    updated

--- web-ui/leptos/leptos-csr/src/main.rs
+++ web-ui/leptos/leptos-csr/src/main.rs
@@ -13,8 +13,24 @@ fn App() -> impl IntoView
     let (count, set_count) = create_signal(0);
 
     view! {
-        <button on:click=move |_| { set_count(3); }>
-            "Click me: " {move || count()}
+        <button on:click=move |_| { set_count.update(|n| *n += 1); }>
+            "Click me"
         </button>
+        <p>
+            <strong>"Reactive: " </strong>
+            {move || count.get()}
+        </p>
+        <p>
+            <strong>"Reactive: " </strong>
+            {move || count()}
+        </p>
+        <p>
+            <strong>"Reactive shorthand: "</strong>
+            {count}
+        </p>
+        <p>
+            <strong>"Not reactive: "</strong>
+            {count()}
+        </p>
     }
 }
