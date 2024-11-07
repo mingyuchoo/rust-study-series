8e878cc808377483cd0243d176bd235b21d5027c
mingyuchoo <mingyuchoo@gmail.com>
2024-11-07 13:42:29 +0900

    updated

--- web-ui/leptos/leptos-csr/src/main.rs
+++ web-ui/leptos/leptos-csr/src/main.rs
@@ -3,6 +3,18 @@ use leptos::*;
 fn main()
 {
     mount_to_body(|| {
-        view! { <p>"Hello, world!"</p> }
+        view! { <App/> }
     })
+}
+
+#[component]
+fn App() -> impl IntoView
+{
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button on:click=move |_| { set_count(3); }>
+            "Click me: " {move || count()}
+        </button>
+    }
 }
