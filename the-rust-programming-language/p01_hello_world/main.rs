a59f3d7f846509a67e339d2079b04c82841f8bc6
my-name <my-email@address.com>
2024-11-14 05:30:31 +0900

    updated

--- the-rust-programming-language/p01_hello_world/main.rs
+++ the-rust-programming-language/p01_hello_world/main.rs
@@ -1,28 +1,26 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  println!("Hello, world!");               // Hello, world!
+    println!("Hello, world!"); // Hello, world!
 
-  print!("Hello, world!");                 // Without new line
-  println!();                              // A new line
+    print!("Hello, world!"); // Without new line
+    println!(); // A new line
 
-  println!("Hello, world!\n");             // With new line
+    println!("Hello, world!\n"); // With new line
 
-  println!("{}, {}!", "Hello", "world");   // Hello, world!
+    println!("{}, {}!", "Hello", "world"); // Hello, world!
 
-  println!("{0}, {1}!", "Hello", "world"); // Hello, world!
+    println!("{0}, {1}!", "Hello", "world"); // Hello, world!
 
-  let x = format!("{}, {}!", "Hello", "world");
-  println!("{}", x);                       // Hello, world!
+    let x = format!("{}, {}!", "Hello", "world");
+    println!("{}", x); // Hello, world!
 
-  println!("{:?}", [1, 2, 3]);             // [1, 2, 3]
+    println!("{:?}", [1, 2, 3]); // [1, 2, 3]
 
-  println!("{:#?}", [1, 2, 3]);
-  /*
-  [
-      1,
-      2,
-      3
-  ]
-  */
+    println!("{:#?}", [1, 2, 3]);
+    // [
+    // 1,
+    // 2,
+    // 3
+    // ]
 
-  Ok(())
+    Ok(())
 }
