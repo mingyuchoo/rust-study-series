94fb564c15cec9b3107e7ec2dbf64aab4e8f05e3
my-name <my-email@address.com>
2024-11-16 07:01:53 +0900

    added

---
+++ web-ui/leptos/leptos-csr/src/part1/passing_children_to_components.rs
@@ -0,0 +1,70 @@
+use leptos::*;
+
+#[component]
+pub fn ComponentChildren() -> impl IntoView {
+    view! {
+        <h1>
+            "Compoent Children"
+        </h1>
+        <TakesChildren render_prop=|| view! {<p>"Hi, there!"</p>}>
+            // there get passed to `children`
+            "Some text"
+            <span>
+                "A span"
+            </span>
+        </TakesChildren>
+    }
+}
+
+#[component]
+pub fn TakesChildren<F, IV>(// Take a function (type F) that returns anything
+                            // that can be converted into a View (type IV)
+                            render_prop: F,
+                            // `children` takes the `Children` type
+                            children: Children)
+                            -> impl IntoView
+    where F: Fn() -> IV,
+          IV: IntoView,
+{
+    view! {
+        <h2>
+            "Render Prop"
+        </h2>
+        {render_prop()}
+        <h2>
+            "Children"
+        </h2>
+        {children()}
+    }
+}
+
+#[component]
+pub fn ManipulatingChildren() -> impl IntoView {
+    view! {
+        <h1>
+            "Manipulating Children"
+        </h1>
+        <WrapsChildren>
+            "A"
+            "B"
+            "C"
+        </WrapsChildren>
+    }
+}
+
+#[component]
+pub fn WrapsChildren(children: Children) -> impl IntoView {
+    // Fragment has `nodes` field that contains a Vec<View>
+    let children = children().nodes
+                             .into_iter()
+                             .map(|child| view! {<li>{child}</li>})
+                             .collect_view();
+    view! {
+        <h2>
+            "WrapsChildren"
+        </h2>
+        <ul>
+            {children}
+        </ul>
+    }
+}
---
+++ web-ui/leptos/leptos-csr/src/part1/reactivity/working_with_signals.rs
@@ -0,0 +1,32 @@
+use leptos::*;
+
+#[component]
+pub fn GettingAndSetting() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    set_count(1);
+    set_count.set(2);
+    set_count.update(|n| *n = 3);
+
+    logging::log!("{}", count());
+
+    let (first, _) = create_signal("Bob".to_string());
+    let (middle, _) = create_signal("J.".to_string());
+    let (last, _) = create_signal("Smith".to_string());
+
+    let name1 = move || first.with(|first| middle.with(|middle| last.with(|last| format!("{first} {middle} {last}"))));
+    let name2 = move || with!(|first, middle, last| format!("{first} {middle} {last}"));
+
+    view! {
+        <div>
+            <h1>
+                "Getting and Setting"
+            </h1>
+            <p>"Count: " {count}</p>
+            <p>"Count: " {count.get()}</p>
+            <p>"Count: " {count.with(|n| n.clone())}</p>
+            <p>{name1}</p>
+            <p>{name2}</p>
+        </div>
+    }
+}
