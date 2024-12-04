35a12b48cac6c7447153394a64fb4871fe1cebb0
mingyuchoo <mingyuchoo@gmail.com>
2024-12-05 08:44:27 +0900

    updated

--- gui/iced/iced_counter/src/main.rs
+++ gui/iced/iced_counter/src/main.rs
@@ -1,14 +1,4 @@
 use iced::widget::{button, column, text, Column};
-use iced::Center;
-
-pub fn main() -> iced::Result {
-    iced::run("A cool counter", Counter::update, Counter::view)
-}
-
-#[derive(Default)]
-struct Counter {
-    value: i64,
-}
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
@@ -16,7 +6,18 @@ enum Message {
     Decrement,
 }
 
+#[derive(Default)]
+struct Counter {
+    value: i64,
+}
+
 impl Counter {
+    fn view(&self) -> Column<Message> {
+        column![button("Increment").on_press(Message::Increment),
+                text(self.value).size(50),
+                button("Decrement").on_press(Message::Decrement)]
+    }
+
     fn update(&mut self,
               message: Message) {
         match message {
@@ -28,14 +29,8 @@ impl Counter {
             },
         }
     }
+}
 
-    fn view(&self) -> Column<Message> {
-        column![
-            button("Increment").on_press(Message::Increment),
-            text(self.value).size(50),
-            button("Decrement").on_press(Message::Decrement)
-        ]
-        .padding(20)
-        .align_x(Center)
-    }
+pub fn main() -> iced::Result {
+    iced::run("A cool counter", Counter::update, Counter::view)
 }
