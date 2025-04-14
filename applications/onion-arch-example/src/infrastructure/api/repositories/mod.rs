7da375206aebfec5249a32c150b25a0ef61887d3
mingyuchoo <mingyuchoo@gmail.com>
2025-04-15 07:46:28 +0900

    removed InMemory

--- applications/onion-arch-example/src/infrastructure/api/repositories/mod.rs
+++ applications/onion-arch-example/src/infrastructure/api/repositories/mod.rs
@@ -4,45 +4,7 @@
 use crate::domain::services::repositories::models::user::User;
 use crate::domain::services::repositories::user_repository::UserRepository;
 use rusqlite::{Connection, params};
-use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-
-// In-Memory 저장소 구현 (기존)
-pub struct InMemoryUserRepository {
-    users: Arc<Mutex<HashMap<String, User>>>,
-}
-
-impl InMemoryUserRepository {
-    pub fn new() -> Self {
-        Self {
-            users: Arc::new(Mutex::new(HashMap::new())),
-        }
-    }
-}
-
-impl UserRepository for InMemoryUserRepository {
-    fn find_by_id(&self, id: &str) -> Option<User> {
-        let users = self.users.lock().unwrap();
-        users.get(id).cloned()
-    }
-
-    fn save(&self, user: &User) -> Result<(), String> {
-        let mut users = self.users.lock().unwrap();
-        users.insert(user.id.clone(), user.clone());
-        Ok(())
-    }
-
-    fn delete(&self, id: &str) -> Result<(), String> {
-        let mut users = self.users.lock().unwrap();
-        users.remove(id);
-        Ok(())
-    }
-
-    fn find_all(&self) -> Vec<User> {
-        let users = self.users.lock().unwrap();
-        users.values().cloned().collect()
-    }
-}
 
 // SQLite 저장소 구현 (추가)
 pub struct SqliteUserRepository {
