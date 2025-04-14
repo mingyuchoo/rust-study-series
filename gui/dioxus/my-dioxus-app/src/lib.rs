addc24ce69f574117f08e0fbd6fc5a115ee227dd
mingyuchoo <mingyuchoo@gmail.com>
2025-04-15 03:11:02 +0900

    updated

--- gui/dioxus/my-dioxus-app/src/lib.rs
+++ gui/dioxus/my-dioxus-app/src/lib.rs
@@ -3,17 +3,3 @@ pub mod application;
 pub mod domain;
 pub mod infrastructure;
 pub mod presentation;
-
-// Import what we need
-use dioxus::prelude::*;
-use crate::application::use_cases::{Route, FAVICON, MAIN_CSS, TAILWIND_CSS};
-
-#[component]
-pub fn App() -> Element {
-    rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        Router::<Route> {}
-    }
-}
--- gui/dioxus/my-dioxus-app/src/main.rs
+++ gui/dioxus/my-dioxus-app/src/main.rs
@@ -1,8 +1,16 @@
-// Only import what we need
+// Import what we need
+use dioxus::prelude::*;
+use my_dioxus_app::application::use_cases::{Route, FAVICON, MAIN_CSS, TAILWIND_CSS};
 
-
-// Import App component from the crate root
-use my_dioxus_app::App;
+#[component]
+pub fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        Router::<Route> {}
+    }
+}
 
 fn main() {
     // Launch the application
