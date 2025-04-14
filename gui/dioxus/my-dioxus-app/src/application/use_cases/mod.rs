562ddd7058faf56111b20e78a004f7deff7a506c
mingyuchoo <mingyuchoo@gmail.com>
2025-04-14 12:53:55 +0900

    updated

--- gui/dioxus/my-dioxus-app/src/application/use_cases/mod.rs
+++ gui/dioxus/my-dioxus-app/src/application/use_cases/mod.rs
@@ -19,12 +19,3 @@ pub const FAVICON: Asset = asset!("/assets/favicon.ico");
 pub const MAIN_CSS: Asset = asset!("/assets/main.css");
 pub const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
-#[component]
-pub fn App() -> Element {
-    rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        Router::<Route> {}
-    }
-}
