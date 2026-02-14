0c08df0b43573f6c6ae1c99710efd686cada9377
PlayaNetworks <playanetworks@gmail.com>
2026-02-15 06:40:03 +0900

    test: Comment out old Vi-style tests in update_tests.rs

--- TUI/ratatui-diary/tests/update_tests.rs
+++ TUI/ratatui-diary/tests/update_tests.rs
@@ -2,23 +2,23 @@ use ratatui_diary::{Model,
                     Msg,
                     model::{EditorMode,
                             Screen},
-                    storage::Storage,
-                    update::Command};
+                    storage::Storage};
 use std::collections::HashSet;
 use tempfile::TempDir;
 
-#[test]
-fn test_calendar_next_month() {
-    let temp = TempDir::new().unwrap();
-    let storage = Storage::with_dir(temp.path()).unwrap();
-    let mut model = Model::new(HashSet::new(), storage);
-    let original_month = model.calendar_state.current_month;
-
-    ratatui_diary::update::update(&mut model, Msg::CalendarNextMonth);
-
-    let expected = if original_month == 12 { 1 } else { original_month + 1 };
-    assert_eq!(model.calendar_state.current_month, expected);
-}
+// TODO: Helix 스타일로 재작성 예정
+// #[test]
+// fn test_calendar_next_month() {
+//     let temp = TempDir::new().unwrap();
+//     let storage = Storage::with_dir(temp.path()).unwrap();
+//     let mut model = Model::new(HashSet::new(), storage);
+//     let original_month = model.calendar_state.current_month;
+//
+//     ratatui_diary::update::update(&mut model, Msg::CalendarNextMonth);
+//
+//     let expected = if original_month == 12 { 1 } else { original_month + 1 };
+//     assert_eq!(model.calendar_state.current_month, expected);
+// }
 
 #[test]
 fn test_calendar_select_date_switches_to_editor() {
@@ -32,18 +32,19 @@ fn test_calendar_select_date_switches_to_editor() {
     assert!(cmd.is_some());
 }
 
-#[test]
-fn test_editor_insert_mode() {
-    let temp = TempDir::new().unwrap();
-    let storage = Storage::with_dir(temp.path()).unwrap();
-    let mut model = Model::new(HashSet::new(), storage);
-    model.screen = Screen::Editor;
-    model.editor_state.mode = EditorMode::Normal;
-
-    ratatui_diary::update::update(&mut model, Msg::EditorEnterInsertMode);
-
-    assert_eq!(model.editor_state.mode, EditorMode::Insert);
-}
+// TODO: Helix 스타일로 재작성 예정
+// #[test]
+// fn test_editor_insert_mode() {
+//     let temp = TempDir::new().unwrap();
+//     let storage = Storage::with_dir(temp.path()).unwrap();
+//     let mut model = Model::new(HashSet::new(), storage);
+//     model.screen = Screen::Editor;
+//     model.editor_state.mode = EditorMode::Normal;
+//
+//     ratatui_diary::update::update(&mut model, Msg::EditorEnterInsertMode);
+//
+//     assert_eq!(model.editor_state.mode, EditorMode::Insert);
+// }
 
 #[test]
 fn test_editor_insert_char() {
